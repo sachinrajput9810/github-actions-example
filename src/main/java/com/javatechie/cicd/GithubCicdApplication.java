9c122dc080cf4dda9c560cceebce6c5ec95818ad
sachinrajput9810 <rajputsachin9810@gmail.com>
2025-08-06 14:02:15 +0530

    code for ci cd flow

---
+++ src/main/java/com/javatechie/cicd/GithubCicdApplication.java
@@ -0,0 +1,26 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdApplication {
+
+	@Getmapping("/welcome")
+	public String welcome() {
+		return "Welcome to JavaTechie";
+	}
+
+//	echo "# github-actions-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/sachinrajput9810/github-actions-example.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdApplication.class, args);
+	}
+
+}
